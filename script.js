b59aa193760d11be74929512a110a7e07033d0f1
sandboi6942 <158761514+sandboi6942@users.noreply.github.com>
2024-02-18 11:34:19 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var trauma = window.prompt("Warning - the following email may contain sensitive and traumatising information. Would you like to proceed? (Yes/no)")
+if (trauma == yes || trauma == Yes || trauma == YES) {
+    alert("Alright then...")
+} else {
+    alert("Don't worry")
+    alert("If you haven't gotten it by now...")
+    alert("Your choices don't matter")
+}
